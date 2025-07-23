f8cff4496a8beead831a9c2164d0c806ee16fec8
alperkangal <alper.polat@paypandas.com>
2025-07-23 15:28:24 +0300

    yeni tasarım

--- Program.cs
+++ Program.cs
@@ -35,8 +35,8 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+//app.MapStaticAssets();
+app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
